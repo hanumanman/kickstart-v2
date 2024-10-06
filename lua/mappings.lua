95703b89a6fdee2f925b84f9a320cbf802a27b69
itztla <hanumanman37@gmail.com>
2024-10-06 15:36:07 +0700

    add turbo omega console log

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -14,3 +14,5 @@ map('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
 map('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 map('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 map('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+map('n', '<leader>l', 'yiwoconsole.log("Logging <C-r>0", <C-r>0)<Esc>', { desc = ' Turbo log' })
