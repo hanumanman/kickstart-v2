d70b8ceec758d76a6973564d0dcaf1077c6cecd6
itztla <hanumanman37@gmail.com>
2024-10-06 08:32:05 +0700

    remove gitblame plugin
    gitsigns already has it

--- lua/plugins/gitblame.lua
+++ lua/plugins/gitblame.lua
@@ -1,11 +1,11 @@
 return {
-  'f-person/git-blame.nvim',
-  config = function()
-    require('gitblame').setup {
-      enabled = false,
-      date_format = '%r',
-    }
-    require('lua-timeago').set_language(require 'lua-timeago/languages/en')
-  end,
-  keys = { { '<leader>gb', '<cmd>GitBlameToggle<cr>', desc = 'Toggle Git Blame' } },
+  -- 'f-person/git-blame.nvim',
+  -- config = function()
+  --   require('gitblame').setup {
+  --     enabled = false,
+  --     date_format = '%r',
+  --   }
+  --   require('lua-timeago').set_language(require 'lua-timeago/languages/en')
+  -- end,
+  -- keys = { { '<leader>gb', '<cmd>GitBlameToggle<cr>', desc = 'Toggle Git Blame' } },
 }
