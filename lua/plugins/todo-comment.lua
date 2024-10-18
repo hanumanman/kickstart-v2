a1912fd07038de757dd9d7022ff43b39b71b2647
itztla <hanumanman37@gmail.com>
2024-10-18 22:19:31 +0700

    reformat todo-comments

--- lua/plugins/todo-comment.lua
+++ lua/plugins/todo-comment.lua
@@ -1,4 +1,9 @@
 return {
-  -- Highlight todo, notes, etc in comments
-  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+  --TODO Highlight todo, notes, etc in comments
+  {
+    'folke/todo-comments.nvim',
+    event = 'VimEnter',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    opts = { signs = true, highlight = { multiline = false, after = '', pattern = [[.*<(KEYWORDS)\s]] } },
+  },
 }
