6931df4d3793b6e11ce2227baef98242d4b8d419
itztla <hanumanman37@gmail.com>
2024-10-28 11:28:20 +0700

    enhance comment function

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -49,7 +49,7 @@ return {
       },
     },
     -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-    build = 'make BUILD_FROM_SOURCE=true',
+    build = 'make',
     -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
     dependencies = {
       'nvim-treesitter/nvim-treesitter',
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -38,4 +38,29 @@ return {
       }
     end,
   },
+  {
+    'numToStr/Comment.nvim',
+    opts = {
+      -- add any options here
+    },
+  },
+  {
+    'JoosepAlviste/nvim-ts-context-commentstring',
+    event = 'BufRead',
+    config = function()
+      -- For native Neovim commenting
+      -- require('ts_context_commentstring').setup {
+      --   enable_autocmd = false,
+      -- }
+      -- local get_option = vim.filetype.get_option
+      -- vim.filetype.get_option = function(filetype, option)
+      --   return option == 'commentstring' and require('ts_context_commentstring.internal').calculate_commentstring() or get_option(filetype, option)
+      -- end
+
+      -- For Comment.nvim
+      require('Comment').setup {
+        pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
+      }
+    end,
+  },
 }
