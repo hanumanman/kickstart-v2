981b5e0bcf8d574342663e63ffad45ff855788cd
itztla <hanumanman37@gmail.com>
2025-01-07 16:50:32 +0700

    chore: cleanup

--- lua/plugins/barbar.lua
+++ lua/plugins/barbar.lua
@@ -29,19 +29,7 @@ return {
       map('n', 'H', '<Cmd>BufferPrevious<CR>', opts)
       map('n', 'L', '<Cmd>BufferNext<CR>', opts)
 
-      -- Goto buffer in position...
-      map('n', '<A-1>', '<Cmd>BufferGoto 1<CR>', opts)
-      map('n', '<A-2>', '<Cmd>BufferGoto 2<CR>', opts)
-      map('n', '<A-3>', '<Cmd>BufferGoto 3<CR>', opts)
-      map('n', '<A-4>', '<Cmd>BufferGoto 4<CR>', opts)
-      map('n', '<A-5>', '<Cmd>BufferGoto 5<CR>', opts)
-      map('n', '<A-6>', '<Cmd>BufferGoto 6<CR>', opts)
-      map('n', '<A-7>', '<Cmd>BufferGoto 7<CR>', opts)
-      map('n', '<A-8>', '<Cmd>BufferGoto 8<CR>', opts)
-      map('n', '<A-9>', '<Cmd>BufferGoto 9<CR>', opts)
-      map('n', '<A-0>', '<Cmd>BufferLast<CR>', opts)
-
-      -- Close buffer
+      -- Close buffer(s)
       map('n', '<leader>x', '<Cmd>BufferClose<CR>', opts)
       map('n', '<leader>bc', '<Cmd>BufferCloseAllButCurrent<CR>', opts)
       map('n', '<leader>br', '<Cmd>BufferCloseBuffersRight<CR>', opts)
