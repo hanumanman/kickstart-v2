66592c30363aed941774f2622c6c211339d6ed21
itz la <itzla@itzs-Mac-mini.local>
2025-03-05 21:14:01 +0700

    chore: remove supermaven

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -82,14 +82,4 @@ return {
       },
     },
   },
-  {
-    'supermaven-inc/supermaven-nvim',
-    config = function()
-      require('supermaven-nvim').setup {
-        keymaps = {
-          accept_suggestion = '<C-k>',
-        },
-      }
-    end,
-  },
 }
