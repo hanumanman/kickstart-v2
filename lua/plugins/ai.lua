e284992e33267f5788dee220887281d40d44852e
itztla <hanumanman37@gmail.com>
2025-03-14 20:56:57 +0700

    copilot new model

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -1,10 +1,22 @@
 return {
   {
     'zbirenbaum/copilot.lua',
+    branch = 'create-pull-request/update-copilot-dist',
     enabled = true,
     event = 'InsertEnter',
-    config = function()
-      require('copilot').setup {
+    config = function(_, opts)
+      local new_opts = vim.tbl_extend('force', opts, {
+        server_opts_overrides = {
+          cmd = {
+            'node',
+            vim.api.nvim_get_runtime_file('copilot/dist/language-server.js', false)[1],
+            '--stdio',
+          },
+          init_options = {
+            copilotIntegrationId = 'vscode-chat',
+          },
+        },
+
         suggestion = {
           enabled = true,
           auto_trigger = true,
@@ -17,7 +29,27 @@ return {
         panel = {
           enabled = false,
         },
-      }
+      })
+
+      require('copilot').setup(new_opts)
+
+      local util = require 'copilot.util'
+      local orig_get_editor_configuration = util.get_editor_configuration
+
+      ---@diagnostic disable-next-line: duplicate-set-field
+      util.get_editor_configuration = function()
+        local config = orig_get_editor_configuration()
+
+        return vim.tbl_extend('force', config, {
+          github = {
+            copilot = {
+              selectedCompletionModel = 'gpt-4o-copilot',
+            },
+          },
+        })
+      end
+
+      return new_opts
     end,
   },
   {
