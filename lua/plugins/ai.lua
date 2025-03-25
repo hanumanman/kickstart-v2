93933aebda208ed0ab19c5256345745f54fc249e
itztla <hanumanman37@gmail.com>
2025-03-25 23:06:06 +0700

    theme change again bc im insane

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -57,8 +57,8 @@ return {
     config = function()
       require('supermaven-nvim').setup {
         keymaps = {
-          accept_suggestion = '<C-k>',
           clear_suggestion = '<C-]>',
+          accept_suggestion = '<C-k>',
           accept_word = '<C-j>',
         },
       }
