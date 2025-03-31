a55981040c66587b955062de1b622a16637353f3
itztla <hanumanman37@gmail.com>
2025-03-31 18:29:37 +0700

    theme: kanagawa

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -2,10 +2,81 @@ local setHighlight = vim.api.nvim_set_hl
 
 local theme = {
   github = false,
-  catppuccin = true,
+  catppuccin = false,
+  kanagawa = true,
 }
 
 return {
+  {
+    'rebelot/kanagawa.nvim',
+    enabled = theme.kanagawa,
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      -- Default options:
+      require('kanagawa').setup {
+        compile = false, -- enable compiling the colorscheme
+        undercurl = true, -- enable undercurls
+        commentStyle = { italic = true },
+        functionStyle = { italic = true },
+        keywordStyle = { italic = true },
+        statementStyle = { bold = true },
+        transparent = true, -- do not set background color
+        dimInactive = false, -- dim inactive window `:h hl-NormalNC`
+        terminalColors = true, -- define vim.g.terminal_color_{0,17}
+        colors = { -- add/modify theme and palette colors
+          palette = {},
+          theme = { wave = {}, lotus = {}, dragon = {}, all = {
+            ui = {
+              bg_gutter = 'none',
+            },
+          } },
+        },
+        overrides = function(colors)
+          local kanagawaTheme = colors.theme
+          local makeDiagnosticColor = function(color)
+            local c = require 'kanagawa.lib.color'
+            return { fg = color, bg = c(color):blend(kanagawaTheme.ui.bg, 0.95):to_hex() }
+          end
+          return {
+            NormalFloat = { bg = 'none' },
+            FloatTitle = { bg = 'none' },
+
+            -- Save an hlgroup with dark background and dimmed foreground
+            -- so that you can use it where your still want darker windows.
+            -- E.g.: autocmd TermOpen * setlocal winhighlight=Normal:NormalDark
+            NormalDark = { fg = kanagawaTheme.ui.fg_dim, bg = kanagawaTheme.ui.bg_m3 },
+
+            -- Popular plugins that open floats will link to NormalFloat by default;
+            -- set their background accordingly if you wish to keep them dark and borderless
+            LazyNormal = { bg = kanagawaTheme.ui.bg_m3, fg = kanagawaTheme.ui.fg_dim },
+            MasonNormal = { bg = kanagawaTheme.ui.bg_m3, fg = kanagawaTheme.ui.fg_dim },
+            Pmenu = { fg = kanagawaTheme.ui.shade0, bg = kanagawaTheme.ui.bg_p1 }, -- add `blend = vim.o.pumblend` to enable transparency
+            PmenuSel = { fg = 'NONE', bg = kanagawaTheme.ui.bg_p2 },
+            PmenuSbar = { bg = kanagawaTheme.ui.bg_m1 },
+            PmenuThumb = { bg = kanagawaTheme.ui.bg_p2 },
+            DiagnosticVirtualTextHint = makeDiagnosticColor(kanagawaTheme.diag.hint),
+            DiagnosticVirtualTextInfo = makeDiagnosticColor(kanagawaTheme.diag.info),
+            DiagnosticVirtualTextWarn = makeDiagnosticColor(kanagawaTheme.diag.warning),
+            DiagnosticVirtualTextError = makeDiagnosticColor(kanagawaTheme.diag.error),
+            SnacksPickerBorder = { fg = '#727169', bg = 'none' },
+            SnacksPickerDir = { fg = '#8B949E', bg = nil, italic = false },
+            FloatBorder = { bg = 'none' },
+          }
+        end,
+        theme = 'wave', -- Load "wave" theme
+        background = { -- map the value of 'background' option to a theme
+          dark = 'wave', -- try "dragon" !
+          light = 'lotus',
+        },
+      }
+
+      -- setup must be called before loading
+      vim.cmd 'colorscheme kanagawa'
+      -- setHighlight(0, 'SnacksPickerDir', { fg = '#8B949E', bg = nil, italic = false })
+      -- setHighlight(0, 'FloatBorder', { fg = '#ffffff', bg = nil })
+    end,
+  },
   {
     'catppuccin/nvim',
     lazy = true,
