9df655082a0d96c72f4bdc7a435cd5fc8b81a1d6
itztla <hanumanman37@gmail.com>
2025-04-03 14:29:16 +0700

    plugins: nvim vtsls

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,6 +1,14 @@
 -- LSP Plugins
 return {
   {
+    'yioneko/nvim-vtsls',
+    event = 'LspAttach',
+    keys = {
+      { '<leader>is', mode = 'n', '<cmd>VtsExec organize_imports<cr>', desc = 'Remove unused import' },
+      { '<leader>ia', mode = 'n', '<cmd>VtsExec add_missing_imports<cr>', desc = 'Add all missing imports' },
+    },
+  },
+  {
     -- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
     -- used for completion, annotations and signatures of Neovim apis
     'folke/lazydev.nvim',
@@ -207,9 +215,6 @@ return {
             },
           },
         },
-        -- denols = {
-        --   root_dir = require('lspconfig.util').root_pattern('deno.json', 'deno.jsonc'),
-        -- },
         vtsls = {
           root_dir = require('lspconfig.util').root_pattern 'package.json',
           single_file_support = true,
