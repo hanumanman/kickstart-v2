1202d564806c60ba6d62bf9947073bb5c4765592
itztla <hanumanman37@gmail.com>
2025-07-19 10:09:16 +0700

    fix markdown preview

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -2,7 +2,7 @@ return {
   {
     'iamcco/markdown-preview.nvim',
     cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
-    build = 'cd app && bun install', -- Install dependencies with bun
+    build = 'cd app && yarn install', -- Install dependencies with yarn
     init = function()
       vim.g.mkdp_filetypes = { 'markdown' }
     end,
